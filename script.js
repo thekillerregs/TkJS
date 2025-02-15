65d2ef7cfdf92203aef8a419d5f208adc883bf23
thekillerregs <thekillerregs@gmail.com>
2025-02-15 14:53:31 -0300

    Enhanced Object Literals

--- script.js
+++ script.js
@@ -22,6 +22,23 @@ const mexicanFoods = new Set([
   'garlic',
 ]);
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  weekdays[3]: {
+    open: 12,
+    close: 22,
+  },
+  weekdays[4]: {
+    open: 11,
+    close: 23,
+  },
+  //ES6 Property name computing
+  weekdays[5]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -30,19 +47,12 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  //ES6 Enhanced object literal
+  openingHours,
+
+  //ES6 Enhanced function declaraction
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
