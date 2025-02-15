48c38a457968f1d470dbcc72360b9e26369e303a
thekillerregs <thekillerregs@gmail.com>
2025-02-15 18:00:34 -0300

    .gitIgnore, script WIP

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 'use strict';
 
-const bookings = [];
-
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-
-  console.log(booking);
-  bookings.push(booking);
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
 };
-
-createBooking('LH123');
