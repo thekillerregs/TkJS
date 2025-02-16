89809af868b6cecfc31100391104f957bfa685ae
thekillerregs <thekillerregs@gmail.com>
2025-02-15 21:13:02 -0300

    Callback Functions

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const oneWord = function (str) {
+const oneWord = function(str) {
   return str.replaceAll(' ', '').toLowerCase();
 };
+
+const upperFirstWord = function(str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function(str, fn) {
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
