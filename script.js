729ef7bebd6782fef9f4dfeeaf8a149a704d1512
thekillerregs <thekillerregs@gmail.com>
2025-02-17 13:18:01 -0300

    Linking prototypes, inheritance between classes

--- script.js
+++ script.js
@@ -5,65 +5,24 @@ const Person = function(firstName, lastName) {
   this.lastName = lastName;
 };
 
-const jonas = new Person('Jonas', 1991);
-
-console.log(jonas instanceof Person);
-
 // Prototypes
 Person.prototype.calcAge = function() {
   console.log(2003);
 };
 
-jonas.calcAge();
-
-console.log(jonas.__prototype__ === Person.prototype);
-
-console.log(Person.prototype.isPrototypeOf(jonas));
-
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species);
-
-console.log(jonas.hasOwnProperty('species'));
-console.log(jonas.hasOwnProperty('firstName'));
-
-// Declaration:
-// const PersonCl = class { }
-
-class PersonCl {
-  constructor(name, birthYear) {
-    this.name = name;
-    this.birthYear = birthYear;
-  }
-
-  calcAge() {
-    console.log(this.birthYear);
-  }
-
-  get latest() {
-    return this.name.slice(-1).pop;
-  }
-
-  set latest(name) {
-    this.name = name;
-  }
-
-  static hey() {
-    console.log('OIIIIIIIII :3');
-  }
-}
-
-const tk = new PersonCl('tk', 2003);
-tk.latest = 'thekllerregs';
-console.log(tk.latest);
-
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
+const Student = function(firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
 };
 
-const tk2 = Object.create(PersonProto);
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
 
-tk2.name = 'tk fofinho';
-tk.birthYear = 2003;
-tk2.calcAge();
+Student.prototype.introduce = function() {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+
+
