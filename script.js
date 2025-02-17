ed97d6a07dab238d41ca9e28850cfcb2d1ef3c73
thekillerregs <thekillerregs@gmail.com>
2025-02-16 22:39:29 -0300

    Prototypes! :3

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const Person = function(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+const jonas = new Person('Jonas', 1991);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function() {
+  console.log(2003);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__prototype__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(jonas.species);
+
+console.log(jonas.hasOwnProperty('species'));
+console.log(jonas.hasOwnProperty('firstName'));
+
