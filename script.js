55fa27aa3a64a26900065daa61cce8418b083eeb
thekillerregs <thekillerregs@gmail.com>
2025-02-17 13:27:30 -0300

    Extension with Object.create/Prototypes

--- script.js
+++ script.js
@@ -1,40 +1,20 @@
 'use strict';
 
-class PersonCl {
-  constructor(name, birthYear) {
-    this.name = name;
-    this.birthYear = birthYear;
-  }
-
+const PersonProto = {
   calcAge() {
-    console.log(this.birthYear);
+    console.log(2037);
+  },
+
+  init(firstName) {
+    this.firstName = firstName;
   }
+};
 
-  get latest() {
-    return this.name.slice(-1).pop;
-  }
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, course) {
+  PersonProto.init.call(this, firstName);
+  this.course = course;
+};
 
-  set latest(name) {
-    this.name = name;
-  }
-
-  static hey() {
-    console.log('OIIIIIIIII :3');
-  }
-}
-
-class StudentCl extends PersonCl {
-  constructor(name, birthYear, course) {
-    super(name, birthYear);
-    this.course = course;
-  }
-
-  introduce() {
-    console.log('oii');
-  }
-}
-
-const tk = new StudentCl('tk', 2003, 'law');
-
-tk.introduce();
-tk.calcAge();
+const tk2 = Object.create(StudentProto);
+tk2.init('tk', 'law');
